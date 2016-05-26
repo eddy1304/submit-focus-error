2456ace23e9a48042b3c11e389e557aeac1193c7
Edward paulino <edward.xuluc@creatibot.com>
2016-05-25 21:18:51 -0500

    inicializacion de la directiva

---
+++ README.md
@@ -0,0 +1,13 @@
+## DESCRIPCIÓN
+
+Esta es una utilidad para enviar el foco del cursor al primer elemento del formulario que tenga un error de validación.
+
+## EJEMPLO
+
+<form name="formularioName" ng-submit="submit()" submit-validate submit-error-focus>
+	
+</form>
+
+## INSTALACIÓN
+
+
---
+++ submit-error-focus.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('submit.error.focus', [])
+
+.directive('submitErrorFocus', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem) {
+
+            // set up event handler on the form element
+            elem.on('submit', function () {
+
+                // find the first invalid element
+                var firstInvalid = elem[0].querySelector('.ng-invalid');
+
+                // if we find one, set focus
+                if (firstInvalid) {
+                    firstInvalid.focus();
+                }
+            });
+        }
+    };
+});
